1d907a618ff297159338f831ce08807deafc1611
ehsan souri <ehsansouri23@gmail.com>
2017-12-13 23:16:18 +0330

    printing provided services function added in agency classs

--- src/Agency.java
+++ src/Agency.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 public class Agency {
     private String name;
     ArrayList<Service> providedServices;
+    ArrayList<Service> orders;
     Agency next;
 
     public Agency(String name) {
@@ -20,6 +21,32 @@ public class Agency {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public void printServices() {
+        for (int i = 0; i < providedServices.size(); i++) {
+            System.out.println("Service: " + providedServices.get(i).getName());
+            printServices(providedServices.get(i).innerList, 1);
+            System.out.println("----------------------------------------");
+        }
+    }
+
+    private void printServices(Service head, int n) {
+        Service p = head;
+        for (; p != null; p = p.next)
+            if (p.getTag() == 0) {
+                System.out.print(p.getName());
+                if (p.next != null)
+                    System.out.print(",");
+            } else {
+                System.out.print(p.getName() + ",");
+                System.out.println();
+                for (int i = 0; i < n; i++) {
+                    System.out.print("      ");
+                }
+                printServices(p.innerList, n + 1);
+            }
+        System.out.println("    ");
     }
 
 }
