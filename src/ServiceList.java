4aae9328a6a49ba1899cbf0c3771673b16099758
ehsan souri <ehsansouri23@gmail.com>
2017-12-13 21:05:24 +0330

    search service function added

--- src/ServiceList.java
+++ src/ServiceList.java
@@ -23,21 +23,70 @@ public class ServiceList {
         }
     }
 
-    public void addSubService(String subServiceName, String serviceName) {
-        Service subService = new Service(subServiceName);
+     public Service findService(Service head, String serviceName) {
         Service p = head;
-        for (; p != null; p = p.next) {
-            if (p.getName().equals(serviceName)) {
-                p.setTag(1);
-                Service q = p.innerList;
-                if (q == null) {
-                    p.innerList = subService;
-                } else {
-                    for (; q.next != null; q = q.next) /*System.out.println(q.getName());*/;
-                    q.next = subService;
-                }
-            }
+        if (p == null)
+            return null;
+//        System.out.println("////////////////////////");
+//        testprint(head);
+//        System.out.println("////////////////////////");
+//        for (; p != null; p = p.next) {
+//            System.out.println(p.getName() + "," + p.getTag() + " is checked");
+//        if (p.getName().equals(serviceName)) {
+//            return p;
+//        } else {
+//            if (p.getTag() == 1) {
+//                Service v = findService(p.innerList, serviceName);
+//                if (v == null && p.next != null)
+//                    return findService(p.next, serviceName);
+//            }
+//            else if (p.next != null)
+//                return findService(p.next, serviceName);
+//
+//        }
+
+
+
+         while (p != null) {
+             if (p.getName().equals(serviceName))
+                 return p;
+             if (p.getTag() == 1) {
+                 if (findService(p.innerList, serviceName) != null)
+                     return findService(p.innerList, serviceName);
+             }
+             p = p.next;
+         }
+         return null;
+//        }
+
+
+
+//         if(head.getName().equals(serviceName))
+//             return head;
+//         else if (head.getTag() == 1)
+//             return findService(head.innerList, serviceName);
+//         else return findService(head.next, serviceName);
+//        return null;
+    }
+
+    public void addSubService(String subServiceName, String serviceName) {
+//        System.out.println("in add sub func ! ");
+        Service subService = new Service(subServiceName);
+        Service p = findService(head, serviceName);
+        if (p == null) {
+            System.out.println("Not such a service named " + serviceName);
+            return;
         }
+        p.setTag(1);
+        Service q = p.innerList;
+        if (q == null) {
+            p.innerList = subService;
+        } else {
+            for (; q.next != null; q = q.next) /*System.out.println(q.getName() + " in add function");*/ ;
+            q.next = subService;
+        }
+
+
     }
 
     public void deleteService(String serviceName) {
@@ -52,7 +101,7 @@ public class ServiceList {
         }
         Service p = head;
         Service q = head.next;
-        for (; q != null && !q.getName().equals(serviceName); q = q.next, p = p.next);
+        for (; q != null && !q.getName().equals(serviceName); q = q.next, p = p.next) ;
         if (q.next == null) {
             p.next = null;
             return;
@@ -64,17 +113,24 @@ public class ServiceList {
         printall(head);
     }
 
-   private void printall(Service head) {
+    private void printall(Service head) {
+        Service p = head;
+        System.out.print("<");
+        for (; p != null; p = p.next)
+            if (p.getTag() == 0) {
+                System.out.print(p.getName());
+                if (p.next != null)
+                    System.out.print(",");
+            } else {
+                System.out.print(p.getName() + ",");
+                printall(p.innerList);
+            }
+        System.out.print(">");
+    }
+
+    public void testprint(Service head) {
         Service p = head;
         for (; p != null; p = p.next)
-            if (p.getTag() == 0)
-                System.out.print(p.getName() + ",");
-            else {
-                System.out.print("<");
-//                printall(p.innerList);
-                Service q = p.innerList;
-                for (; q != null; q = q.next) System.out.print(q.getName() + ",");
-                System.out.print(">");
-            }
+            System.out.println(p.getName() + "," + p.getTag());
     }
 }
