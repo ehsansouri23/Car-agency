1151b154b1736078925af7132d6bcfdda2292996
ehsan souri <ehsansouri23@gmail.com>
2017-12-13 22:56:45 +0330

    find agency function added to agency list

--- src/AgencyList.java
+++ src/AgencyList.java
@@ -10,22 +10,26 @@ public class AgencyList {
         this.serviceList = serviceList;
     }
 
+    public Agency getHead() {
+        return head;
+    }
+
     public void addAgency(String agencyName) {
         Agency agency = new Agency(agencyName);
         if (head == null)
             head = agency;
         else {
             Agency p = head;
-            for (; p.next != null; p = p.next);
+            for (; p.next != null; p = p.next) ;
             p.next = agency;
         }
     }
 
     public void addOffer(String serviceName, String agencyName) {
         Service p = serviceList.getHead();
-        for (; p != null && !p.getName().equals(serviceName); p = p.next);
+        for (; p != null && !p.getName().equals(serviceName); p = p.next) ;
         Agency q = head;
-        for (; q != null && !q.getName().equals(agencyName); q = q.next);
+        for (; q != null && !q.getName().equals(agencyName); q = q.next) ;
         p.setNumberOfProviders(p.getNumberOfProviders() + 1);
         q.providedServices.add(p);
     }
@@ -35,17 +39,26 @@ public class AgencyList {
         Service beforeP = serviceList.getHead();
         for (; p != null && !p.getName().equals(serviceName); p = p.next) ;
         Agency q = head;
-        for (; q != null && !q.getName().equals(agencyName); q = q.next);
+        for (; q != null && !q.getName().equals(agencyName); q = q.next) ;
         if (p.getNumberOfProviders() > 1) {
             p.setNumberOfProviders(p.getNumberOfProviders() - 1);
             return;
-        }
-        else {
+        } else {
             serviceList.deleteService(serviceName);
         }
     }
 
-    public void printAll(){
+    public Agency findAgency(Agency head, String agencyName) {
+        Agency p = head;
+        while (p != null) {
+            if (p.getName().equals(agencyName))
+                return p;
+            p = p.next;
+        }
+        return null;
+    }
+
+    public void printAll() {
         Agency p = head;
         for (; p != null; p = p.next)
             System.out.println(p.getName());
